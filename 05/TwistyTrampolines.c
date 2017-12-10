a14e999cc7275d9c788e89082145be6bda6872da
P1n3appl3 <josephryan3.14@gmail.com>
2017-12-10 03:21:46 -0600

    Clean redundant loops

--- 05/TwistyTrampolines.c
+++ 05/TwistyTrampolines.c
@@ -19,35 +19,20 @@ int main(){
     memcpy(listcopy, list, 2000 * sizeof(int));
 
     struct timeb start, end;
-    float diff;
-    ftime(&start);
-    int place = 0;
-    int steps = 0;
-    int temp;
-    while (place >= 0 && place < len) {
-        temp = place;
-        place += list[place];
-        ++list[temp];
-        ++steps;
+    for (int i = 1; i < 3; i++) {
+        memcpy(list, listcopy, 2000 * sizeof(int));
+        ftime(&start);
+        int place = 0;
+        int steps = 0;
+        while (place >= 0 && place < len) {
+            int temp = place;
+            place += list[place];
+            list[temp] += 1 - 2 * (list[temp] > 2) * (i == 2);
+            ++steps;
+        }
+        ftime(&end);
+        float diff = (end.millitm - start.millitm);
+        printf("Part %d: %d\tTime: %1.3f\n", i, steps, diff / 1000);
     }
-    ftime(&end);
-    diff = (end.millitm - start.millitm);
-    printf("Part 1: %d\t\tTime: %1.3f\n", steps, diff/1000);
-
-    memcpy(list, listcopy, 2000 * sizeof(int));
-
-    ftime(&start);
-    place = 0;
-    steps = 0;
-    while (place >= 0 && place < len) {
-        temp = place;
-        place += list[place];
-        list[temp] += 1 - 2 * (list[temp] > 2);
-        ++steps;
-    }
-    ftime(&end);
-    diff = (end.millitm - start.millitm);
-    printf("Part 2: %d\tTime: %1.3f\n", steps, diff/1000);
-
     return 0;
 }
