38c074616b6f4831c19c72e68c488809e9c7cc61
Joseph Ryan <josephryan3.14@gmail.com>
2018-12-01 00:56:10 -0600

    Start 2018!

---
+++ 2018/01/main.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let mut contents = String::new();
+    let mut f = File::open("input.txt").unwrap();
+    f.read_to_string(&mut contents).unwrap();
+    let numbers: Vec<i32> = contents
+        .lines()
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+    println!("{}", numbers.iter().sum::<i32>());
+
+    let mut seen = HashSet::new();
+    let mut current = 0;
+    'bigloop: loop {
+        for num in numbers.iter() {
+            seen.insert(current);
+            current += num;
+            if seen.contains(&current) {
+                println!("Saw {} for a second time", current);
+                break 'bigloop;
+            }
+        }
+    }
+}
