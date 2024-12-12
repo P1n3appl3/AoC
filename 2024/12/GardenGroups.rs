6d7a84a2d1534945d1c0e9003aed42f26b789740
Julia Ryan <p1n3appl3@users.noreply.github.com>
2024-12-12 00:28:55 -0800

    Day 12 (part 2)

--- 2024/12/GardenGroups.rs
+++ 2024/12/GardenGroups.rs
@@ -10,23 +10,47 @@ fn main() {
         })
         .collect();
     let mut todo: BTreeSet<_> = garden.keys().copied().collect();
-    let mut total = 0;
+    let (mut p1, mut p2) = (0, 0);
     while let Some(pos) = todo.first().copied() {
-        let (area, perimeter) = fill(pos, garden[&pos], &mut todo, &garden);
-        total += area * perimeter;
+        let mut edges = BTreeSet::new();
+        let mut plant = BTreeSet::new();
+        let (area, perimeter) =
+            fill(pos, garden[&pos], &mut todo, &mut plant, &mut edges, &garden);
+        p1 += area * perimeter;
+        p2 += area * sides(&edges, &plant);
+        // println!("{}: area: {} perimeter: {} sides: {}",
+        //     garden[&pos] as char, area, perimeter, sides(&edges, &plant))
     }
-    println!("{total}");
+    println!("{p1}");
+    println!("{p2}");
 }
 
 #[rustfmt::skip]
 fn fill(
     pos @ (x, y): (i16, i16), plant: u8,
-    todo: &mut BTreeSet<(i16, i16)>, garden: &HashMap<(i16, i16), u8>,
+            todo: &mut BTreeSet<(i16, i16)>,
+             cur: &mut BTreeSet<(i16, i16)>,
+           edges: &mut BTreeSet<(i16, i16)>,
+          garden:      &HashMap<(i16, i16), u8>,
 ) -> (u32, u32) {
-    let Some(&p) = garden.get(&pos) else { return (0, 1) };
-    if p != plant { return (0, 1); };
+    let Some(&p) = garden.get(&pos) else { edges.insert(pos); return (0, 1); };
+                    /*||*/ if p != plant { edges.insert(pos); return (0, 1); };
     if !todo.remove(&pos) { return (0, 0); };
-    [(-1, 0), (0, -1), (1, 0), (0, 1)].iter()
-        .map(|(dx, dy)| fill((x + dx, y + dy), p, todo, garden))
+    cur.insert(pos);
+    [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        .iter()
+        .map(|(dx, dy)| fill((x + dx, y + dy), p, todo, cur, edges, garden))
         .fold((1, 0), |(x, y), (dx, dy)| (x + dx, y + dy))
+}
+
+#[rustfmt::skip]
+fn sides(edges: &BTreeSet<(i16, i16)>, plant: &BTreeSet<(i16, i16)>) -> u32 {
+    edges.iter().copied().flat_map(|(x, y)| { [
+        plant.contains(&(x + 1, y    )) && !(plant.contains(&(x + 1, y + 1)) && edges.contains(&(x,     y + 1))),
+        plant.contains(&(x,     y + 1)) && !(plant.contains(&(x + 1, y + 1)) && edges.contains(&(x + 1, y    ))),
+        plant.contains(&(x - 1, y    )) && !(plant.contains(&(x - 1, y - 1)) && edges.contains(&(x,     y - 1))),
+        plant.contains(&(x,     y - 1)) && !(plant.contains(&(x - 1, y - 1)) && edges.contains(&(x - 1, y    ))),
+    ].into_iter() })
+    .filter(|&b| b)
+    .count() as u32
 }
