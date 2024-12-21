41332dad4ab6bb3e0c8e859c9df342f5bf4a4860
Julia Ryan <p1n3appl3@users.noreply.github.com>
2024-12-21 04:49:18 -0800

    Day 21 (part 2)

--- 2024/03/brrrrr/mine.rs
+++
@@ -1,1 +0,0 @@
-../MullItOver.rs
--- 2024/21/KeypadConundrum.py
+++ 2024/21/KeypadConundrum.py
@@ -1,70 +1,42 @@
 from functools import cache
 
 with open("input") as f:
-    raw = f.read()
-numpad = {
+    codes = f.read().splitlines()
+pad = lambda s: {
     c: (x, y)
-    for y, line in enumerate("789\n456\n123\n.0A".splitlines())
+    for y, line in enumerate(s.splitlines())
     for x, c in enumerate(line)
 }
-dirpad = {
-    c: (x, y)
-    for y, line in enumerate(".^A\n<v>".splitlines())
-    for x, c in enumerate(line)
-}
-codes = raw.splitlines()
-
-dist = lambda a, b: abs(a[0] - b[0]) + abs(a[1] - b[1])
+numpad = pad("789\n456\n123\n.0A")
+dirpad = pad(".^A\n<v>")
 diff = lambda a, b: (b[0] - a[0], b[1] - a[1])
+strip_num = lambda s: int("".join((c for c in s if c.isdigit())))
 
 
 @cache
-def move(s, p2=False, level=0):
-    if not p2 and level == 2 or p2 and level == 25:
-        return s
-    pos = dirpad["A"]
-    path = ""
+def shortest(s, target, level=-1):
+    if level == target:
+        return len(s)
+    cur = numpad["A"] if level < 0 else dirpad["A"]
+    total = 0
     for c in s:
-        new = dirpad[c]
-        dx, dy = diff(pos, new)
-        hori = "<" if dx < 0 else ">"
-        vert = "^" if dy < 0 else "v"
-        a = move(hori * abs(dx) + vert * abs(dy) + "A", p2, level + 1)
-        b = move(vert * abs(dy) + hori * abs(dx) + "A", p2, level + 1)
-        if pos[0] == 0 and new[1] == 0:
-            path += a
-        elif pos[1] == 0 and new[0] == 0:
-            path += b
+        dx, dy = diff(cur, new := numpad[c] if level < 0 else dirpad[c])
+        hori = ("<" if dx < 0 else ">") * abs(dx)
+        vert = ("^" if dy < 0 else "v") * abs(dy)
+        recurse = lambda s: shortest(s + "A", target, level + 1)
+        a, b = recurse(hori + vert), recurse(vert + hori)
+        if    (level <  0 and cur[0] == 0 and new[1] == 3
+            or level >= 0 and cur[0] == 0 and new[1] == 0):
+            total += a
+        elif  (level <  0 and cur[1] == 3 and new[0] == 0
+            or level >= 0 and cur[1] == 0 and new[0] == 0):
+            total += b
         else:
-            path += a if len(a) < len(b) else b
-        pos = new
-    return path
+            total += min(a, b)
+        cur = new
+    return total
 
 
-def solve(p2):
-    ans = 0
-    for code in codes:
-        print(".", end="")
-        path = ""
-        pos = numpad["A"]
-        for c in code:
-            new = numpad[c]
-            dx, dy = diff(pos, new)
-            hori = "<" if dx < 0 else ">"
-            vert = "^" if dy < 0 else "v"
-            a = move(abs(dx) * hori + abs(dy) * vert + "A", p2)
-            b = move(abs(dy) * vert + abs(dx) * hori + "A", p2)
-            if pos[0] == 0 and new[1] == 3:
-                path += a
-            elif pos[1] == 3 and new[0] == 0:
-                path += b
-            else:
-                path += a if len(a) < len(b) else b
-            pos = numpad[c]
-        ans += len(path) * int("".join((c for c in code if c.isdigit())))
-    print()
-    return ans
-
-
-print(solve(False))
-print(solve(True))
+solve = lambda n: sum((shortest(code, n) * strip_num(code) for code in codes))
+print(solve(2))
+print(solve(25))
